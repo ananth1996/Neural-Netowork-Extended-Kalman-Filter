2f7ecabdc218aed1c75ca8fc705470fd57311b2a
Ananth Mahadevan <ananth1996@gmail.com>
2019-04-16 20:57:28 +0300

    First Commit

---
+++ .ipynb_checkpoints/load-checkpoint.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def load_abalone_data():
+    columns = ['sex','length','diameter','height','weight','meat_weight','viscera_weight','shell_weight','rings']
+    df = pd.read_csv('Data/abalone.data',header=None,names=columns)
+    df['sex']=df.sex.astype('category').cat.codes
+    y = df.pop('rings').values
+    X = df.values
+    return X,y
+
+def load_bikes_data():
+    df = pd.read_csv('Data/Bikes_hour.csv',index_col='instant')
+    df = df.drop(columns='dteday')
+    y = df.pop('cnt').values
+    X = df.values
+    return X,y
